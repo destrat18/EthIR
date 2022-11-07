f672c1191a4b48ac8c9b125922a805668b816b10
destrat18 <destrat18@gmail.com>
2022-11-07 10:30:34 +0000

    nothing

--- helper.py
+++ helper.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
-import json, logging, pickle, os
+import json, logging, pickle, os, shutil
 from slither.slither import Slither
 from slither_utils import generate_initial_state, get_function_list, get_sol_summary
 import subprocess
+
+COSTABS_PATH = '/tmp/costabs'
 
 if __name__ == '__main__':
 
@@ -10,17 +12,24 @@ if __name__ == '__main__':
     
     parser = ArgumentParser()
     parser.add_argument("-s", "--source", help="Solidity file")
+    parser.add_argument("-o", "--output", help="Output directory")
     args = parser.parse_args()
     
     meta = {}
     slither_obj = Slither(args.source)  
+    
+    # create output directory
+    if not args.output:
+        args.output = os.path.dirname(args.source)
+    else:
+        args.output = os.path.join(args.output, os.path.basename(args.source).replace(".sol", ""))
+    os.makedirs(args.output, exist_ok=True)
 
     # generate rbr
     subprocess.run(
         [
             'python', "ethir/ethir.py",
-            "-s", args.source,
-            "--saco"
+            "-s", args.source
             ],
     )
 
@@ -36,7 +45,7 @@ if __name__ == '__main__':
         meta[contract.name]['initial_state'] = generate_initial_state(contract)    
 
         # read function mapping    
-        fun_map_file_path = os.path.join("/tmp/costabs", contract.name+'.fun_map')
+        fun_map_file_path = os.path.join(COSTABS_PATH, contract.name+'.fun_map')
         with open(fun_map_file_path, 'rb') as handle:
             fun_map = pickle.load(handle)
             meta[contract.name]['function_block_mapping'] = {
@@ -44,11 +53,27 @@ if __name__ == '__main__':
             } 
     
         # read call opcode names
-        call_fun_file_path = os.path.join("/tmp/costabs", contract.name+'.call_fun')
+        call_fun_file_path = os.path.join(COSTABS_PATH, contract.name+'.call_fun')
         with open(call_fun_file_path, 'rb') as handle:
             call_fun = pickle.load(handle)
             meta[contract.name]['block_CALL_mapping'] = call_fun    
 
-    meta_output = args.source.replace(".sol", ".meta")
+    # copy source file
+    shutil.copy(
+        args.source,
+        os.path.join(args.output, os.path.basename(args.source))
+    )
+    # copy rbr files
+    for f in os.listdir(COSTABS_PATH):
+        if f.endswith('.rbr'):
+            shutil.copy(
+                os.path.join(COSTABS_PATH, f),
+                os.path.join(args.output, f)
+            )
+
+    meta_output = os.path.join(
+        args.output,
+        os.path.basename(args.source).replace(".sol", ".meta")
+    )
     with open(meta_output, 'w') as handle:
         json.dump(meta, handle, indent=4)
